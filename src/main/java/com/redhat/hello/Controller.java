82f681fc8087aed16e85eaee61bd84de3aff1076
tgubeli <jtomasg@gmail.com>
2021-06-17 11:36:11 -0400

    Initial commit

--- src/main/java/com/redhat/hello/Controller.java
+++ src/main/java/com/redhat/hello/Controller.java
@@ -1,11 +1,12 @@
 package com.redhat.hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    @RequestMapping("/hello", method = RequestMethod.GET, produces = { "application/json" })
+    @RequestMapping(value="/hello", method = RequestMethod.GET, produces = { "application/json" })
     public String hello() {
         return "{'greetings':'Hello world!'}";
     }
